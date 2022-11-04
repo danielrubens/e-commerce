6953d5fbf755437b2c56a81e378ed844959923a6
danielrubens <danielrubensdaniel@gmail.com>
2022-11-04 01:17:44 -0300

    Footer icons

--- ecommerce/components/Navbar.jsx
+++ ecommerce/components/Navbar.jsx
@@ -5,16 +5,15 @@ import {AiOutlineShopping} from 'react-icons/ai';
 const Navbar = () => {
   return (
     <div className='navbar-container'>
-      <p className='logo'>
-        <Link href="/">JSM Headphones</Link>  
-      </p>
-
-    <button
-    type="button"
-    className="cart-icon"
-    onClick="">
-      <AiOutlineShopping />
-      <span className='cart-item-qty'>{1}</span>
+       <p className='logo'>
+          <Link href="/">JSM Headphones</Link>  
+       </p>
+      <button
+          type="button"
+          className="cart-icon"
+          onClick="">
+          <AiOutlineShopping />
+          <span className='cart-item-qty'>{1}</span>
       </button>
     </div>
   )
