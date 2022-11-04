9afb94d584ec5126499020e81d98a031c41fa94c
danielrubens <danielrubensdaniel@gmail.com>
2022-11-03 23:21:26 -0300

    banner and image from Home component

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,21 +1,33 @@
 import React from 'react'
+import { client } from '../lib/client';
 import {Product, FooterBanner, HeroBanner} from '../components';
-const Home = () => {
+
+const Home = ({products, bannerData}) => {
   return (
     <>
-    HeroBanner
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
     <div className='products-heading'>
-      <h2>Best Selling Products</h2>
+      <h2>Best Seller Products</h2>
       <p>Speaker of any variations</p>
     </div>
 
     <div className='products-container'>
-      {['Product1', 'Product2'].map((i) => i)}
+      {products?.map((i) => i.name)}
     </div>
-    Footer
+    <FooterBanner/>
     </>
     
   )
+}
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query)
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery)
+  return {
+    props: {products, bannerData}
+  }
 }
 
 export default Home
