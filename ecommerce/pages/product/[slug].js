32532333b3c83e8eeb91bd7a44bcde5436e2ef4a
danielrubens <danielrubensdaniel@gmail.com>
2022-11-04 13:57:05 -0300

    Product details. Add to cart

--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {client, urlFor} from '../../lib/client'
+import {AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar} from 'react-icons/ai';
 
 const ProductDetails = ({product, products}) => {
     const {image, name, details, price} = product
@@ -20,6 +21,43 @@ const ProductDetails = ({product, products}) => {
 
                   ))}
                 </div> */}
+            </div>
+            <div className='product-details-desc'>
+                <h1>{name}</h1>
+                <div className='reviews'>
+                  <div>
+                    <AiFillStar />
+                    <AiFillStar />
+                    <AiFillStar />
+                    <AiFillStar />
+                    <AiFillStar />
+                    <AiOutlineStar />
+                  </div>
+                <p>(20)</p>
+                </div>
+                <h4>Details: </h4>
+                <p>{details}</p>
+                <p className='price'>${price}</p>
+                <div className='quantity'>
+                  <h3>Quantity: </h3>
+                  <p className='quantity-desc'>
+                    <span className='minus' onClick=''>
+                      <AiOutlineMinus />
+                    </span>
+                    <span className='num' onClick=''>
+                      0
+                    </span>
+                    <span className='plus' onClick=''>
+                      <AiOutlinePlus />
+                    </span>
+                  </p>
+                </div>
+                <div className='buttons'>
+                  <button 
+                    type="button"
+                    className='add-to-cart'
+                    onClick=''>Add to Cart</button>
+                </div>
             </div>
         </div>
     </div>
